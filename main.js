087269bd600fca029d6e1c50cfb7acfc1abe9ab3
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-10-13 14:22:25 -0400

    almost completed all equations

--- main.js
+++ main.js
@@ -2,6 +2,65 @@
  * YOUR CODE HERE *
  ******************/
 
+function onlyOdds(nums) {
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 1) {
+      result.push(nums[i]);
+    }
+  }
+  
+  return result;
+
+}
+
+function onlyEvens(nums) {
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      result.push(nums[i]);
+    }
+  }
+  return result;
+}
+
+function shortNamesOnly(names) {
+  const result = [];
+  
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i]
+    console.log(name.length);
+    if (names[i].length < 7) {
+      result.push(names[i]);
+    }
+  }
+  return result;
+}
+
+function sNames(names) {
+  const result = [];
+
+  for (let i = 0; i < names.length; i++) {
+    console.log(result);
+    if (names[i].upperCase + 'S') {
+      result.push(names[i]);
+    }
+  }
+  return result;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
